c084513f2555ad996d3e4182f0fa51355902d8b7
Sreram Rathnam <52123952+sreramr@users.noreply.github.com>
2024-08-14 22:08:38 +0530

    Update orion_node.py

--- collections/ansible_collections/solarwinds/orion/plugins/modules/orion_node.py
+++ collections/ansible_collections/solarwinds/orion/plugins/modules/orion_node.py
@@ -238,6 +238,7 @@ orion_node:
 
 from datetime import datetime, timedelta
 import requests
+import urllib3
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.solarwinds.orion.plugins.module_utils.orion import OrionModule, orion_argument_spec
 try:
